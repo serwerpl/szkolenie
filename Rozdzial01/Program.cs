f1def728396bd8b0d857d26d11a283fcba02da44
serwerpl <poiuuhb@wp.pl>
2019-04-07 22:15:45 +0200

    Pierwsza wersja aplikacji

---
+++ Rozdzial01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rozdzial01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witam cyganko!");
+        }
+    }
+}
